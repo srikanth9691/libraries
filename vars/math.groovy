540eb6b870702f7fbdbf287e5297c7af8dec3637
srikanth9691 <137414542+srikanth9691@users.noreply.github.com>
2024-02-01 19:39:22 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+  echo "the sum of ${a} and${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo "the subtration of${a} and ${b} is ${a-b}"
+}
+def mul(a,d)
+{
+  echo " the product of ${a} and ${b} is ${a*b}"
+}
+def div(a,b)
+{
+  echo "the divison of ${a} and ${b} is ${a/b}"
+}
