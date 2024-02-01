1c54ed468749269be2f12b9c4172dd54232a9fe4
srikanth9691 <137414542+srikanth9691@users.noreply.github.com>
2024-02-01 20:48:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,6 +8,6 @@ def newMaven()
 }
 def newDeploy(jobname,ip,context)
 {
-   sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war'
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
-
+def newTesting
